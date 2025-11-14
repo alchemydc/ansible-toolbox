7a8bfe1d67e9f3b8c2c63a50b4db1186a8ab9862
DC <alchemydc@proton.me>
2025-11-14 12:36:06 -0700

    refactor(proxmox_hypervisor): improved swap task.
    will create a new partition using available free space on disks defined as swap devices and format and activate.
    
    will not do anything if swap partition(s) already exist.

--- roles/proxmox_hypervisor/files/restart_pve_services.sh
+++ roles/proxmox_hypervisor/files/restart_pve_services.sh
@@ -6,3 +6,7 @@ sudo systemctl restart pvedaemon
 sudo systemctl restart pvestatd 
 sudo systemctl restart pve-cluster 
 sudo systemctl restart pveproxy 
+# restart new nftables based firewall service
+sudo systemctl restart proxmox-firewall
+# restart the legacy iptables based firewall service
+# sudo systemctl restart pve-firewall
